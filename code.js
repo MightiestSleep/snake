9bcc9c4941f1e59676de3a3919804de759f4a2b2
MightySleep <jaredhenrysleep@gmail.com>
2023-05-26 14:35:26 -0600

    Finished! Simple snake game using HTML, CSS, and Javascript principles. No frameworks used to make this happen.

--- code.js
+++ code.js
@@ -1,11 +1,55 @@
-function controller()
+function genApple()
 {
-    findNextID(CURRENT_POSITION,MOVEMENT);
+    var randomNumber = Math.floor(Math.random()*576);
+    let apple = "#id_" + randomNumber.toString();
+    const the_cell = document.querySelector(apple);
+    if(the_cell.style.background == "white")
+    {
+        
+        the_cell.style.background = 'red';
+    }
+    else
+    {
+        genApple();
+    }
     
 }
-function findNextID(last_id,action)
+function controller()
 {
-    let id_number = last_id.substring(4);
+    if(PLAY)
+    {
+        findNextID(MOVEMENT);
+    }
+    
+    
+}
+function reset_game()
+{
+    const cells = document.querySelectorAll('.cell');
+
+    for(let i = 0; i<cells.length;i++)
+    {
+        
+        cells[i].style.background = "white";
+
+    }
+    queue.length = 3;
+    queue.push('#id_55');
+    queue.push('#id_79');
+    queue.push('#id_103');
+    MOVEMENT = "down";
+    genApple();
+    queue.length = 3;
+    CURRENT_POSITION = queue[queue.length-1];
+    PLAY = true;
+    
+    SCORE = 0;
+    SCR.innerHTML = "Score: " + SCORE;
+}
+
+function findNextID(action)
+{
+    let id_number = queue[queue.length-1].substring(4);
     //alert(id_number);
     id_number = parseInt(id_number);
     //alert(id_number);
@@ -18,11 +62,40 @@ function findNextID(last_id,action)
         }
         next_id = '#id_' + next_id_number.toString();
        // alert(next_id);
+
+
+        
+
+        /*
         const wha = document.querySelector(CURRENT_POSITION);
         wha.style.background = 'white';
-        const what = document.querySelector(next_id);
-        what.style.background = 'black';
-        CURRENT_POSITION = next_id;
+        */
+        
+        const next_item = document.querySelector(next_id);
+        // alert(next_item.style.background);
+        if(next_item.style.background == "red")
+        {
+            next_item.style.background = 'black';
+            queue.push(next_id);
+            SCORE = SCORE + 1;
+            SCR.innerHTML = "Score: " + SCORE.toString();
+            genApple();
+        }
+        else if(next_item.style.background == 'black')
+        {
+            PLAY = false;
+            SCR.innerHTML = "Game over!";
+        }
+
+        else {
+            next_item.style.background = 'black';
+                // push new position into queue
+            queue.push(next_id);
+            // make last element white again
+            var i = queue.shift();
+            const abtr = document.querySelector(i);
+            abtr.style.background = 'white';
+        }
     }
 
     if(action == "up")
@@ -34,11 +107,31 @@ function findNextID(last_id,action)
         }
         next_id = '#id_' + next_id_number.toString();
        // alert(next_id);
-        const wha = document.querySelector(CURRENT_POSITION);
-        wha.style.background = 'white';
-        const what = document.querySelector(next_id);
-        what.style.background = 'black';
-        CURRENT_POSITION = next_id;
+       const next_item = document.querySelector(next_id);
+       // alert(next_item.style.background);
+       if(next_item.style.background == "red")
+       {
+           next_item.style.background = 'black';
+           queue.push(next_id);
+           genApple();
+           SCORE = SCORE + 1;
+           SCR.innerHTML = "Score: " + SCORE.toString();
+       }
+       else if(next_item.style.background == 'black')
+        {
+            PLAY = false;
+            SCR.innerHTML = "Game over!";
+        }
+       else {
+           next_item.style.background = 'black';
+               // push new position into queue
+           queue.push(next_id);
+           // make last element white again
+           var i = queue.shift();
+           const abtr = document.querySelector(i);
+           abtr.style.background = 'white';
+       }
+
     }
 
     if(action == "left")
@@ -50,11 +143,32 @@ function findNextID(last_id,action)
         }
         next_id = '#id_' + next_id_number.toString();
        // alert(next_id);
-        const wha = document.querySelector(CURRENT_POSITION);
-        wha.style.background = 'white';
-        const what = document.querySelector(next_id);
-        what.style.background = 'black';
-        CURRENT_POSITION = next_id;
+        
+       const next_item = document.querySelector(next_id);
+       // alert(next_item.style.background);
+       if(next_item.style.background == "red")
+       {
+           next_item.style.background = 'black';
+           queue.push(next_id);
+           genApple();
+           
+           SCORE = SCORE + 1;
+           SCR.innerHTML = "Score: " + SCORE.toString();
+       }
+       else if(next_item.style.background == 'black')
+        {
+            PLAY = false;
+            SCR.innerHTML = "Game over!";
+        }
+       else {
+           next_item.style.background = 'black';
+               // push new position into queue
+           queue.push(next_id);
+           // make last element white again
+           var i = queue.shift();
+           const abtr = document.querySelector(i);
+           abtr.style.background = 'white';
+       }
     }
 
     if(action == "right")
@@ -65,65 +179,112 @@ function findNextID(last_id,action)
             next_id_number = next_id_number - 24;
         }
         next_id = '#id_' + next_id_number.toString();
-       // alert(next_id);
-        const wha = document.querySelector(CURRENT_POSITION);
-        wha.style.background = 'white';
-        const what = document.querySelector(next_id);
-        what.style.background = 'black';
-        CURRENT_POSITION = next_id;
+        
+       const next_item = document.querySelector(next_id);
+       if(next_item.style.background == "red")
+       {
+           next_item.style.background = 'black';
+           queue.push(next_id);
+           genApple();
+           SCORE = SCORE + 1;
+           SCR.innerHTML = "Score: " + SCORE.toString();
+       }
+       else if(next_item.style.background == 'black')
+        {
+            PLAY = false;
+            SCR.innerHTML = "Game over!";
+        }
+       else {
+           next_item.style.background = 'black';
+               // push new position into queue
+           queue.push(next_id);
+           // make last element white again
+           var i = queue.shift();
+           const abtr = document.querySelector(i);
+           abtr.style.background = 'white';
+       }
+
     }
 
 
 }
-let CURRENT_POSITION = '#id_28';
+
 let MOVEMENT = 'down';
+
+var queue = [];
+queue.push('#id_55');
+queue.push('#id_79');
+queue.push('#id_103');
+let CURRENT_POSITION = queue[queue.length-1];
+let SCORE = queue.length - 3;
+let PLAY = true;
+let SPEED = 60;
+
 
 const container = document.querySelector('.container');
 for(let i=0;i<576;i++)
 {
     const cell = document.createElement('div');
     cell.id = "id_"+i.toString();
+    cell.style.background = 'white';
     cell.classList.add('cell');
     container.appendChild(cell);
 }
 
-const cells = document.querySelectorAll('.cell');
 
-for(let i = 0; i<cells.length;i++)
-{
-    
-    // cells[i].addEventListener('mouseover',makeSolid);
-    cells[i].addEventListener('mouseover', function (e) {
-        e.target.style.background = "black";
-    });
+const wsg = document.querySelector('#id_55');
+wsg.style.background = 'black';
+const ctm = document.querySelector(queue[1]);
+ctm.style.background = 'black';
+const btb = document.querySelector(queue[2]);
+btb.style.background = 'black';
+genApple();
 
-}
-
-const test = document.querySelector('#id_28');
-test.style.background = 'black';
 
 document.addEventListener("keydown",function(e) {
     if(e.key == "s")
     {
         
-        MOVEMENT = 'down';
+        if(MOVEMENT != 'up')
+        {
+            MOVEMENT = 'down';
+        }
+        
     }
     if(e.key == "w")
     {
         //alert("Found it");
-        MOVEMENT = 'up';
+        if(MOVEMENT != 'down')
+        {
+            MOVEMENT = 'up';
+        }
+        
     }
     
     if(e.key == "a")
     {
         //alert("Found it");
-        MOVEMENT = 'left';
+       
+        if(MOVEMENT != 'right')
+        {
+            MOVEMENT = 'left';
+        }
     }
     if(e.key == "d")
     {
         //alert("Found it");
-        MOVEMENT = 'right';
+        if(MOVEMENT != 'left')
+        {
+            MOVEMENT = 'right';
+        }
     }
     
 })
-const the_controller = setInterval(controller, 100);
+
+const btn = document.querySelector('#retry');
+btn.addEventListener('click', reset_game);
+const SCR = document.querySelector('.top');
+SCR.innerHTML = "Score: " + SCORE.toString();
+
+const the_controller = setInterval(controller, SPEED);
+
